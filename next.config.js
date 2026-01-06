cbaa8a854d3cd2bb062e785619357bbf58c82f9e
Joshua Winkler <josh@bemoty.dev>
2026-01-06 14:00:44 +0100

    build: add nextjs security headers

--- next.config.js
+++ next.config.js
@@ -16,6 +16,35 @@ const nextConfig = {
             },
         ],
     },
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'Strict-Transport-Security',
+                        value: 'max-age=63072000; includeSubDomains; preload',
+                    },
+                    {
+                        key: 'X-XSS-Protection',
+                        value: '1; mode=block',
+                    },
+                    {
+                        key: 'X-Frame-Options',
+                        value: 'SAMEORIGIN',
+                    },
+                    {
+                        key: 'X-Content-Type-Options',
+                        value: 'nosniff',
+                    },
+                    {
+                        key: 'Referrer-Policy',
+                        value: 'origin-when-cross-origin',
+                    },
+                ],
+            },
+        ]
+    },
 }
 
 module.exports = nextConfig
